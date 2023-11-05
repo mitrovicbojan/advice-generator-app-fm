d2b6edc2f2e7320ae8bc4a2a2a380e53ddbc388c
unknown <mitrovicbojan87@gmail.com>
2023-11-05 17:53:09 +0200

    api

---
+++ app.js
@@ -0,0 +1,11 @@
+const adviceId = document.getElementById("adviceId");
+const advice = document.getElementById("advice");
+
+async function adviceGenerator() {
+  const response = await fetch("https://api.adviceslip.com/advice");
+  const advices = await response.json();
+  adviceId.textContent = advices.slip.id;
+  advice.textContent = advices.slip.advice;
+}
+
+console.log(adviceGenerator());
