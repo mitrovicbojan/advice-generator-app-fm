51e72597f84f5954258f7e327278a94bf3a5dfe3
unknown <mitrovicbojan87@gmail.com>
2023-11-05 21:07:07 +0200

    css

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const advice = document.getElementById("advice");
 // async function adviceGenerator() {
 //   const response = await fetch("https://api.adviceslip.com/advice");
 //   const advices = await response.json();
-//   adviceId.textContent = advices.slip.id;
-//   advice.textContent = advices.slip.advice;
+//   adviceId.innerHTML = advices.slip.id;
+//   advice.innerHTML = advices.slip.advice;
 // }
 
 console.log(adviceGenerator());
