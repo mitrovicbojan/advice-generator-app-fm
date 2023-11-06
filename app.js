c3a9ed90aec4727e8ef440380a860134539abc50
unknown <mitrovicbojan87@gmail.com>
2023-11-06 21:21:11 +0200

    btn

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const adviceId = document.getElementById("adviceId");
 const advice = document.getElementById("advice");
+const btnDice = document.getElementById("btnDice");
 
-// async function adviceGenerator() {
-//   const response = await fetch("https://api.adviceslip.com/advice");
-//   const advices = await response.json();
-//   adviceId.innerHTML = advices.slip.id;
-//   advice.innerHTML = advices.slip.advice;
-// }
+async function adviceGenerator() {
+  const response = await fetch("https://api.adviceslip.com/advice");
+  const advices = await response.json();
+  adviceId.innerHTML = advices.slip.id;
+  advice.innerHTML = advices.slip.advice;
+}
 
-console.log(adviceGenerator());
+btnDice.addEventListener("click", () => {
+  adviceGenerator();
+});
